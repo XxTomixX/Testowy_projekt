8bf824bd093e64a6f47b8b50161ba283ed67222d
Adam <72575016+adam-glab@users.noreply.github.com>
2020-12-03 16:16:00 +0100

    Dodanie klasy motocykl

--- Przykladowy_program.cpp
+++ Przykladowy_program.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+class Motocykl{
+public:
+	string nazwa;
+	int rocznik;
+};
+
+
 class Samochod {
   public:
     string marka;   
@@ -32,8 +39,13 @@ int main() {
   sam2.marka = "Ford";
   sam2.model = "Mustang";
   sam2.rocznik = 1971;
+	
+  Motocykl mot1;
+  mot1.nazwa="komar";
+  mot1.rocznik="1988";
 
   cout << sam1.marka << " " << sam1.model << " " << sam1.rocznik << "\n";
   cout << sam2.marka << " " << sam2.model << " " << sam2.rocznik << "\n";
+  cout << mot1.nazwa << " " << mot1.rocznik << "\n";
   return 0;
 }
